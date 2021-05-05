28ec4385c265f4c8148629dfbe856b5a63297fb3
Hugo Bernier <hugoabernier@live.ca>
2021-05-04 22:12:15 -0500

    Deployed 7276b35 with MkDocs version: 1.1.2

--- javascript/filtersamples.js
+++ javascript/filtersamples.js
@@ -1,18 +1,39 @@
 // external js: isotope.pkgd.js
 
+/***
+ * This is a generic isotope filter for samples
+ */
+
 $(document).ready(function () {
-  var jsonPath = "https://pnp.github.io/powerplatform-samples/samples.json";
   var filterText = $('#sample-listing').data("filter");
+  var qsRegex;
+  var buttonFilter;
 
   // init Isotope
   var $grid = $('#sample-listing').isotope({
     itemSelector: '.sample-thumbnail',
     layoutMode: 'fitRows',
-    sortBy : 'modified',
+    sortBy: 'modified',
     sortAscending: false,
     getSortData: {
       modified: '[data-modified]',
       title: '.sample-title'
+    },
+    filter: function () {
+      var searchResult = qsRegex ? $(this).data("keywords").match(qsRegex) : true;
+      var buttonResult = buttonFilter ? $(this).is(buttonFilter) : true;
+      return searchResult && buttonResult;
+    }
+  });
+
+  // Display/hide a message when there are no results
+  $grid.on('arrangeComplete', function (_event, filteredItems) {
+    if (filteredItems.length > 0) {
+      // hide message 
+      $("#noresults").hide();
+    } else {
+      // show message; 
+      $("#noresults").show();
     }
   });
 
@@ -22,318 +43,84 @@ $(document).ready(function () {
     var prop = "updateDateTime";
 
     // Sort data descending order
-    data = data.sort(function(a, b) {
+    data = data.sort(function (a, b) {
       try {
         if (asc) return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
-        else return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);  
+        else return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
       } catch (error) {
         return 0;
       }
     });
 
-
     $.each(data, function (_u, sample) {
 
-      try {
-        var title = _.escape(sample.title);
-        var escapedDescription = _.escape(sample.shortDescription);
-        var categories = sample.categories[0];
+      var item = loadSample(sample, filterText);
 
-        var modified = new Date(sample.updateDateTime).toString().substr(4).substr(0, 12);
-
-        var authors = sample.authors;
-        var authorsList = "";
-        var authorAvatars = "";
-
-        authors.forEach(author => {
-          if (authorsList !== "") {
-            authorsList = authorsList + ", ";
-          }
-          authorsList = authorsList + author.name;
-
-          var authorAvatar = `<div class="author-avatar">
-            <div role="presentation" class="author-coin">
-              <div role="presentation" class="author-imagearea">
-                <div class="image-400">
-                  <img class="author-image" src="${author.pictureUrl}" alt="${author.name}" title="${author.name}">
-                </div>
-              </div>
-            </div>
-          </div>`;
-          authorAvatars = authorAvatar + authorAvatars;
-        });
-
-        var authorName = authors[0].name;
-        if (authors.length > 1) {
-          authorName = authorName + ` +${authors.length - 1}`;
-        }
-
-        var tags = "";
-        $.each(sample.tags, function (_u, tag) {
-          tags = tags + "#" + tag + ",";
-        });
-
-        var keywords = title + escapedDescription + authorsList + tags;
-        keywords = keywords.toLowerCase();
-
-        var productTag = "powerfx";
-        var productName = "Power Fx";
-
-        switch (categories) {
-          case "POWERAPPS":
-            productTag = "powerapps";
-            productName = "Power Apps";
-            break;
-
-          default:
-            break;
-        }
-
-        var $items = $(`
-        <a class="sample-thumbnail" href="${sample.url}"  data-modified="${sample.modified}" data-title="${title}" data-keywords="${keywords}" data-tags="${tags}" data-category=${categories}>
-        <div class="sample-inner">
-          <div class="sample-preview">
-            <img src="${sample.thumbnails[0].url}" alt="${title}">
-          </div>
-          <div class="sample-details">
-            <div class="producttype-item ${productTag}">${productName}</div>
-            <p class="sample-title" title="${sample.title}">${sample.title}</p>
-            <p class="sample-description" title='${escapedDescription}'>${sample.shortDescription}</p>
-            <div class="sample-activity">
-              ${authorAvatars}
-              <div class="activity-details">
-              <span class="sample-author" title="${authorsList}">${authorName}</span>
-              <span class="sample-date">Modified ${modified}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </a>`);
-
-        if (filterText !== undefined && filterText !== "" && filterText !== categories) {
-          // Skip this sample as it doesn't meet the filter
-          ;
-        } else {
-          //$grid.isotope( 'appended', elements );
-          $grid.append($items)
-            // add and lay out newly appended items
-            .isotope('appended', $items);
-
-        }
-      } catch (error) {
-        console.log("Error with one sample", error);
+      if (item !== null) {
+        $grid.append(item)
+          // add and lay out newly appended items
+          .isotope('appended', item);
       }
+
     });
 
     // Update the sort
     $grid.isotope('updateSortData').isotope();
   });
 
-  // filter functions
-  var filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function () {
-      var number = $(this).find('.number').text();
-      return parseInt(number, 10) > 50;
-    },
-    // show if name ends with -ium
-    ium: function () {
-      var name = $(this).find('.name').text();
-      return name.match(/ium$/);
-    }
-  };
+  // Get the list of filters to use
+  var filter = $('#filters .filter-choice');
 
-  // bind filter button click
-  $('#filters').on('click', '.filter-choice', function () {
-    var filterValue = $(this).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[filterValue] || filterValue;
-    $grid.isotope({ filter: filterValue });
-  });
-  // change is-checked class on buttons
+  // Get the search box
+  var search = $('#post-search-input');
+
   $('.filter-list').each(function (_i, buttonGroup) {
     var $buttonGroup = $(buttonGroup);
     $buttonGroup.on('click', '.filter-choice', function () {
       $buttonGroup.find('.active').removeClass('active');
       $(this).addClass('active');
+      var filters = [];
+
+      filter.filter('.active').each(function () {
+        filters.push($(this).data("filter"));
+      });
+      //filters = filters.join(', ');    //OR
+      filters = filters.join('');         //AND
+      buttonFilter = filters;
+      $grid.isotope();
+
     });
   });
 
+  search.on('change keyup paste', debounce(function () {
+    qsRegex = new RegExp(search.val(), 'gi');
+    $grid.isotope();
+  }, 200));
 
+  // debounce so filtering doesn't happen every millisecond
+  function debounce(fn, threshold) {
+    var timeout;
+    return function debounced() {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+      function delayed() {
+        fn();
+        timeout = null;
+      }
+      timeout = setTimeout(delayed, threshold || 100);
+    };
+  }
 
-
-
-  //post-search-input
-
-  // // init Isotope -- new
-  // // var $grid = $('#sample-listing').isotope({
-  // //   itemSelector: '.sample-thumbnail',
-  // //   layoutMode: 'fitRows',
-  // //   containerStyle: null,
-  // //   resize: true,
-  // //   initLayout: false
-  // // });
-
-  // // init isotope -- old
-
-  // // init Isotope
-  // var $grid = $('.grid').isotope({
-  //   itemSelector: '.sample-item',
-  //   layoutMode: 'fitRows',
-  //   // sortAscending: false,
-  //   // sortBy: 'modified',
-  //   // getSortData: {
-  //   //   title: '[data-title]',
-  //   //   // number: '.number parseInt',
-  //   //   modified: '[data-modified]'
-  //   // }
-  // });
-
-
-  $("#post-search-input").on('change keyup paste', function () {
-    var selection = $('#post-search-input').val();
-    if (selection !== "") {
-      selection = selection.toLowerCase();
-      $grid.isotope({ filter: `[data-keywords*='${selection}']` });
-    } else {
-      $grid.isotope({ filter: '*' });
+  // See if there are any passed parameters
+  try {
+    var urlParams = new URLSearchParams(window.location.search);
+    var query = urlParams.get('query');
+    if (query !== "") {
+      search.val(query).change();
     }
-  });
 
-  // var filterFns = {
-  //   hasTech: function () {
-  //     return $(this).find(':not([data-technology=""])');
-  //   },
-  //   // show if number is greater than 50
-  //   hasPnPjs: function () {
-  //     var name = $(this).attr('data-technology');
-  //     return name.indexOf('PnPjs') != -1;
-  //   },
-  //   hasCognitive: function () {
-  //     var name = $(this).attr('data-technology');
-  //     return name.indexOf('AzureCognitiveServices') != -1;
-  //   },
-  //   hasBot: function () {
-  //     var name = $(this).attr('data-technology');
-  //     return name.indexOf('AzureBotServices') != -1;
-  //   },
-  //   hasInsights: function () {
-  //     var name = $(this).attr('data-technology');
-  //     return name.indexOf('AzureAppInsights') != -1;
-  //   }, hasFunctions: function () {
-  //     var name = $(this).attr('data-technology');
-  //     return name.indexOf('AzureFunctions') != -1;
-  //   },
-  //   hasGitHub: function () {
-  //     var name = $(this).attr('data-technology');
-  //     return name.indexOf('GitHub') != -1;
-  //   }
-  // };
-
-  // // bind filter button click
-  // $('.filters-button-group').on('click', 'button', function () {
-  //   var filterValue = $(this).attr('data-filter');
-  //   // use filterFn if matches value
-  //   filterValue = filterFns[filterValue] || filterValue;
-  //   $grid.isotope({ filter: filterValue });
-  // });
-  // // change is-checked class on buttons
-  // $('.button-group').each(function (i, buttonGroup) {
-  //   var $buttonGroup = $(buttonGroup);
-  //   $buttonGroup.on('click', 'button', function () {
-  //     $buttonGroup.find('.is-checked').removeClass('is-checked');
-  //     $(this).addClass('is-checked');
-  //   });
-  // });
-
-  // // bind sort button click
-  // $('.sort-button-group').on('click', 'button', function () {
-
-  //   /* Get the element name to sort */
-  //   var sortValue = $(this).attr('data-sort-value');
-
-  //   /* Get the sorting direction: asc||desc */
-  //   var direction = $(this).attr('data-sort-direction');
-
-  //   /* convert it to a boolean */
-  //   var isAscending = (direction == 'asc');
-  //   var newDirection = (isAscending) ? 'desc' : 'asc';
-
-  //   /* pass it to isotope */
-  //   $grid.isotope({ sortBy: sortValue, sortAscending: isAscending });
-
-  //   $(this).attr('data-sort-direction', newDirection);
-  // });
-
-
-  // Trigger animation on GIF hover
-  // $("a img[data-fullsize$='.gif']").hover((e) => {
-  //   // on mouse enter
-  //   var img = e.target;
-  //   var customdata = $(img).data('fullsize');
-  //   $(img).attr('src', customdata);
-  // }, (e) => {
-  //   // on mouse leave
-  //   var img = e.target;
-  //   var customdata = $(img).data('orig');
-  //   $(img).attr('src', customdata);
-  // });
-
-  // $("#author").on('change keyup paste', function () {
-  //   console.log('I am pretty sure the text box changed');
-  //   var selection = $('#author').val();
-  //   if (selection !== "") {
-  //     console.log('Selection', selection, `[data-author*='${selection}']`);
-  //     $grid.isotope({ filter: `[data-author*='${selection}']` });
-  //   } else {
-  //     $grid.isotope({ filter: '*' });
-  //   }
-
-  // });
-
-  // $("#keyword").on('change keyup paste', function () {
-  //   var selection = $('#keyword').val();
-  //   if (selection !== "") {
-  //     $grid.isotope({ filter: `[data-keywords*='${selection}']` });
-  //   } else {
-  //     $grid.isotope({ filter: '*' });
-  //   }
-
-  // });
-
-
-
-  // Make samples data sortal
-  $('#samplestable th').each(function (col) {
-    $(this).hover(
-      function () { $(this).addClass('focus'); },
-      function () { $(this).removeClass('focus'); }
-    );
-    $(this).click(function () {
-      if ($(this).is('.asc')) {
-        $(this).removeClass('asc');
-        $(this).addClass('desc selected');
-        sortOrder = -1;
-      }
-      else {
-        $(this).addClass('asc selected');
-        $(this).removeClass('desc');
-        sortOrder = 1;
-      }
-      $(this).siblings().removeClass('asc selected');
-      $(this).siblings().removeClass('desc selected');
-      var arrData = $('table').find('tbody >tr:has(td)').get();
-      arrData.sort(function (a, b) {
-        var val1 = $(a).children('td').eq(col).text().toUpperCase();
-        var val2 = $(b).children('td').eq(col).text().toUpperCase();
-        if ($.isNumeric(val1) && $.isNumeric(val2))
-          return sortOrder == 1 ? val1 - val2 : val2 - val1;
-        else
-          return (val1 < val2) ? -sortOrder : (val1 > val2) ? sortOrder : 0;
-      });
-      $.each(arrData, function (index, row) {
-        $('tbody').append(row);
-      });
-    });
-  });
+  } catch (error) {
+    // Be vewy vewy quiet
+  }
 });
