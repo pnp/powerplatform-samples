ae497daaf8488802f6df76d07cbd39b82b322bf5
Iona <46789417+Ionavoss@users.noreply.github.com>
2023-12-13 09:34:14 +0100

    Update DeployBicep.ps1

--- samples/live-avatars/assets/DeployBicep.ps1
+++ samples/live-avatars/assets/DeployBicep.ps1
@@ -1,14 +1,14 @@
 # Parameters
-$resourceGroupName = "avatar-demo-group"
-$bicepFilePath = "live-avatar.bicep"
-$avatar_name  = 'ionaavatardemo'
-$openai_instance_name  = 'openaiforavatardemo'
-$openai_deploymentname  = 'avatarname'
+$resourceGroupName = "avatar-demo-group" # Change to the name of your resource group
+$bicepFilePath = "live-avatar.bicep" # # Change to the name of your path
+$avatar_name  = 'ionaavatardemo' # Change to the name of your liking for the Avatar text-to-speech services
+$openai_instance_name  = 'openaiforavatardemo' # Change to the name you want for the OpenAI instance
+$openai_deploymentname  = 'avatarname' # Change to the name of your OpenAI model deployment
 
 # Install Az module and log on to Azure
 Install-Module -Name Az -AllowClobber -Force -Scope CurrentUser
-Connect-AzAccount -TenantId 8ed42453-85a2-4957-935b-a70352369dc2 
-Set-AzContext -Subscription 51e2a291-75d0-42b8-a4e4-477e1922dfbd
+Connect-AzAccount -TenantId 39c9ce23-27e9-464a-8a10-30ea6a6ec26f # Change to your Azure tenant ID
+Set-AzContext -Subscription 39c9ce23-27e9-464a-8a10-30ea6a6ec26f # Change to your subscription ID, this step may be obsolete when only 1 subscription is present
 
 # Deploy Bicep file with overridden parameter
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $bicepFilePath -avatar_name $avatar_name -openai_instance_name $openai_instance_name -openai_deploymentname $openai_deploymentname
